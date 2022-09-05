96a7c8ec43d959b283c598e49207d51e689e746c
micko-123 <mickyasne123@gmail.com>
2022-09-05 06:07:22 -0700

    controler updated

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,27 +1,59 @@
 const asyncHandler = require('express-async-handler')
 
+const Employee = require('../models/employeeModel')
+const Admin = require('../models/adminModel')
 
 const getEmployee = asyncHandler( async (req, res) => {
-    res.send('geting employee')
+    const employees = await Employee.find()
+
+  res.status(200).json(employees)
+
 })
 
-
 const addEmployee =  asyncHandler( async(req, res) => {
-    res.send('adding employee')
+   
+      const goal = await Goal.create({
+        lastName: req.body.lastName,
+        firstName: req.body.firstName,
+        email:req.body.email,
+        gender:req.body.gender,
+        adress:req.body.adress,
+        dateOfBirth:req.body.dateOfBirth,
+        phoneNumber: req.body.phoneNumber,
+        admin: req.admin.id,
+      })
+    
+      res.status(200).json(goal)
 })
 
 
 const updateEmployee = asyncHandler( async (req, res) => {
-    res.send('updating employee')
+    const employee = await Employee.findById(req.params.id)
+
+  if (!employee) {
+    res.status(400)
+    throw new Error('employee not found')
+  }
+
 })
 
 
 const deleteEmployee = asyncHandler( async (req, res) => {
-    res.send('deleting employee')
+    const employee = await Employee.findById(req.params.id)
+
+    if (!employee) {
+      res.status(400)
+      throw new Error('employee not found')
+    }
+  
+    await employee.remove()
+  
+    res.status(200).json({ id: req.params.id })
+  
 })
 
 
-module.exports ={
+module.exports = {
     getEmployee,
     addEmployee,
     updateEmployee,
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorHandler')
 const port = process.env.PORT || 5000;
-
+const connectDB = require('./config/db')
 const app = express();
+
+connectDB() 
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
